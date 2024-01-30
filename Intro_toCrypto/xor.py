ee247159036b790dde630df68f34b9a430d086eb
Dhruva Deepak <61975172+kua23@users.noreply.github.com>
2024-01-30 08:27:53 +0530

    Update xor.py

--- Intro_toCrypto/xor.py
+++ Intro_toCrypto/xor.py
@@ -12,6 +12,7 @@ for x in word:
     flag += chr((ord(x)^13)) #Here, XORing is done by first converting the string to its ASCII, and then XORing the ASCII with the number 13. This is then converted back to a string at the end
 print(flag)
 
-
+''' Flag
+    crypto{Aloha} '''
 
 
