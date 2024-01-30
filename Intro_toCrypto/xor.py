5e663fc63fccae2c01c57e00a05e943a592b2284
Dhruva Deepak <61975172+kua23@users.noreply.github.com>
2024-01-30 08:25:27 +0530

    Add files via upload

---
+++ Intro_toCrypto/xor.py
@@ -0,0 +1,17 @@
+
+''' from pwn import *
+from Crypto.Util.number import *
+word = "label"
+integer = 13
+flag = xor(word, integer)
+print(flag) '''
+
+word = "label"
+flag = ""
+for x in word:
+    flag += chr((ord(x)^13)) #Here, XORing is done by first converting the string to its ASCII, and then XORing the ASCII with the number 13. This is then converted back to a string at the end
+print(flag)
+
+
+
+
